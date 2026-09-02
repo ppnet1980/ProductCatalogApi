929191d041da9e65d5a6c1da204c0c91316d342f
ppnet1980 <110386478+ppnet1980@users.noreply.github.com>
2026-09-02 23:25:25 +0200

    Update ProductsController.cs
    Added Delete method

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -44,4 +44,19 @@ public class ProductsController : ControllerBase
 
         return CreatedAtAction(nameof(GetById), new { id = createdProduct.Id }, createdProduct);
     }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        var product = Products.FirstOrDefault(p => p.Id == id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        Products.Remove(product);
+
+        return NoContent();
+    }    
 }
